cf7c1980aa01b838f79bfa294096d18804be403e
nathan joseph <josephnathan10@gmail.com>
2021-01-29 12:32:21 -0900

    finished assessment

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import NotFoundPage from "./components/404page/404page";
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Nathan" />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/welcome/:name" component={Welcome} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
